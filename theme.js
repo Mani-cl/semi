76041120cc1bf262e6ff89194b9ce828e9a66e5a
Mani-cl <117332325+Mani-cl@users.noreply.github.com>
2022-12-20 21:50:00 +0200

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,10 @@
+
+const english= document.getElementById("english");
+const tamil= document.getElementById("tamil");
+const russian= document.getElementById("russian");
+const malayalam= document.getElementById("malayalam");
+const language={
+    ru:{
+        
+    }
+}
